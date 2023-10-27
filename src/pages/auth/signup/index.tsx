b7ab3a7ace11d87130a2733e3f83f0d96b77f6e2
Fiquell <fiquellh@gmail.com>
2023-10-28 04:19:45 +0800

    rewrite registration pages

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,143 +1,137 @@
-import ilustration from "@/img/Illustration.png";
-import {
-  Button,
-  Card,
-  Checkbox,
-  Input,
-  Typography,
-} from "@material-tailwind/react";
+import Ellipse from "@/img/Ellipse 859.png";
+import Illustration from "@/img/Illustration.png";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Button, Input, Typography } from "@material-tailwind/react";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Register() {
+export default function SignUp() {
   return (
-    <>
-      <div className="container flex justify-center items-center">
-        <div className="flex gap-10 items-center justify-center">
-          <div className=" w-1/2 my-52">
-            <div className="flex flex-col bg-primary w-96 h-screen">
-              <div className="">
-                <Typography variant="paragraph" className="text-4xl ">
-                  To simplest way to manage your money
-                </Typography>
-                <Typography>
-                  {" "}
-                  Enter your email to access your account
-                </Typography>
-              </div>
-              <div>
-                <Image
-                  src={ilustration}
-                  width={250}
-                  height={250}
-                  priority
-                  alt="Picture of the author"
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <Card color="transparent" shadow={false} className="mt-5">
-              <div className="flex flex-col items-center gap-2">
-                <Typography variant="h2" color="blue-gray">
-                  Create Account
-                </Typography>
-                <Typography color="gray" className="mt-1 font-normal">
-                  Nice to meet you! Enter your details to register.
-                </Typography>
-              </div>
-              <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-                <div className="mb-1 flex flex-col gap-3">
-                  <Typography variant="h6" color="blue-gray" className="-mb-3">
-                    Your Name
-                  </Typography>
-                  <Input
-                    crossOrigin={""}
-                    size="md"
-                    placeholder="your name"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  />
-                  <Typography variant="h6" color="blue-gray" className="-mb-3">
-                    Your Email
-                  </Typography>
-                  <Input
-                    crossOrigin={""}
-                    size="md"
-                    placeholder="name@mail.com"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  />
-                  <Typography variant="h6" color="blue-gray" className="-mb-3">
-                    Password
-                  </Typography>
-                  <Input
-                    crossOrigin={""}
-                    type="password"
-                    size="md"
-                    placeholder="********"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  />
-                  <Typography variant="h6" color="blue-gray" className="-mb-3">
-                    Confirm Password
-                  </Typography>
-                  <Input
-                    crossOrigin={""}
-                    type="password"
-                    size="md"
-                    placeholder="********"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  />
-                </div>
-                <Checkbox
-                  crossOrigin={""}
-                  label={
-                    <Typography
-                      variant="small"
-                      color="gray"
-                      className="flex items-center font-normal"
-                    >
-                      I agree the
-                      <Link
-                        href="#"
-                        className="font-medium transition-colors hover:text-gray-900"
-                      >
-                        &nbsp;Terms and Conditions
-                      </Link>
-                    </Typography>
-                  }
-                  containerProps={{ className: "-ml-2.5" }}
-                />
-                <Button className="mt-6 bg-secondary" fullWidth>
-                  sign up
-                </Button>
-                <Typography
-                  color="gray"
-                  className="mt-4 text-center font-normal"
-                >
-                  Already have an account?{" "}
-                  <Link
-                    href="/form/SignIn"
-                    className="font-medium text-gray-900"
-                  >
-                    Sign In
-                  </Link>
-                </Typography>
-              </form>
-            </Card>
-          </div>
+    <section className="container mx-auto flex my-10 items-center justify-center gap-28">
+      <div className="bg-primary p-20 rounded-lg w-1/2">
+        <Typography variant="h3" color="white">
+          The simplest way to manage your money
+        </Typography>
+        <Typography variant="paragraph" color="white">
+          Enter your email to access your account
+        </Typography>
+        <div className="flex flex-col items-center my-16">
+          <Image
+            src={Illustration}
+            alt="Illustration"
+            width={450}
+            height={460}
+          />
+          <Image
+            src={Ellipse}
+            alt="Ellipse"
+            width={250}
+            height={75}
+            className="-mt-8"
+          />
         </div>
       </div>
-    </>
+      <div className="w-1/2 flex flex-col items-center gap-5 px-10">
+        <div className="text-center">
+          <Typography variant="h2">Create Account</Typography>
+          <Typography variant="h5">Please enter your detail</Typography>
+        </div>
+        <form className="flex flex-col gap-5 w-full">
+          <div>
+            <Typography variant="h6">First name*</Typography>
+            <Input
+              type="text"
+              size="lg"
+              placeholder="Username"
+              className="focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              icon={
+                <Icon
+                  icon="material-symbols:account-circle"
+                  className="text-xl text-gray-900"
+                />
+              }
+              crossOrigin={undefined}
+            />
+          </div>
+          <div>
+            <Typography variant="h6">Email*</Typography>
+            <Input
+              type="email"
+              size="lg"
+              placeholder="Email"
+              className="focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              icon={
+                <Icon
+                  icon="material-symbols:mark-email-unread"
+                  className="text-xl text-gray-900"
+                />
+              }
+              crossOrigin={undefined}
+            />
+          </div>
+          <div>
+            <Typography variant="h6">Password*</Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="Password"
+              className="focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              icon={
+                <Icon
+                  icon="material-symbols:password-rounded"
+                  className="text-xl text-gray-900"
+                />
+              }
+              crossOrigin={undefined}
+            />
+          </div>
+          <div>
+            <Typography variant="h6">Confirm Password*</Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="Confirm Password"
+              className="focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              icon={
+                <Icon
+                  icon="material-symbols:password-rounded"
+                  className="text-xl text-gray-900"
+                />
+              }
+              crossOrigin={undefined}
+            />
+          </div>
+          <Button type="submit" size="lg" fullWidth className="bg-primary">
+            Create Account
+          </Button>
+        </form>
+        <Button
+          size="lg"
+          fullWidth
+          className="bg-secondary flex items-center gap-2 justify-center text-black"
+        >
+          <Icon icon="logos:google-icon" className="text-xl" />
+          Log in With Google
+        </Button>
+        <Typography variant="small" color="gray" className="w-full mt-5">
+          Already have an account?{" "}
+          <Link href="/auth/signin" className="text-black font-bold">
+            Log in
+          </Link>
+        </Typography>
+      </div>
+    </section>
   );
 }
